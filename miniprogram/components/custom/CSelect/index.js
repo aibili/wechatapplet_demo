20d6edba621aaa0947aeae021ec91f8299c35192
aibili <442757052@163.com>
2019-10-18 19:20:47 +0800

    feat: 筛选组件,还没完成

---
+++ miniprogram/components/custom/CSelect/index.js
@@ -0,0 +1,32 @@
+// components/custom/CSelect/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    show: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    showPopup(){
+      this.setData({
+        show: true
+      });
+    },
+    onClose(){
+      this.setData({
+        show: false
+      });
+    }
+  }
+})
