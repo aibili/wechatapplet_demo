0800b094b3ac4313d614a81958cbbb9fa4070cc6
aibili <442757052@163.com>
2019-11-13 23:06:27 +0800

    feat: 编辑页面未完成

--- miniprogram/pages/design/index.js
+++ miniprogram/pages/design/index.js
@@ -1,4 +1,7 @@
+import {designData} from '../../dictionary/index';
 const app = getApp();
+
+
 Page({
 
   /**
@@ -6,7 +9,8 @@ Page({
    */
   data: {
     navbarHeight: app.globalData.navbarHeight,
-    detail:[]
+    detail:[],
+    maskFlag: false
   },
 
   /**
@@ -16,75 +20,24 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  openMask(){
+    this.setData({
+      maskFlag: !this.data.maskFlag
+    });
   },
   selectPart(e){
+    //对应组件的类型
     let type = e.currentTarget.dataset.type;
+    //对应组件的数据结构
+    let item = designData[type];
+
+    console.log(type)
+    console.log(item)
+    //这里不采用直接覆盖整个数组的方式，性能不太好
+    //而是采用对应索引号更新的方式
     this.setData({
-      detail:[{
-        componentName:'cswiper',
-        cofig:{
-          type:'3'
-        },
-        detail:[{
-          id: "fd88909b29d84ad388197a04a384ca5d",
-          src: "https://qwyimg.do1.com.cn/fileweb/upload/img/81b69b451f5d4b13adce0d8687dc0c02/20191010/6e5e4943a2a3451db7cc28d60d609608.jpeg"
-        },
-        {
-          id: "fd88909b29d84ad388197a04a384ca5d",
-          src: "https://qwyimg.do1.com.cn/fileweb/upload/img/81b69b451f5d4b13adce0d8687dc0c02/20191010/c6a78b4b2d384be78c489bb4d0521d05.jpeg"
-        },
-        {
-          id: "fd88909b29d84ad388197a04a384ca5d",
-          src: "https://qwyimg.do1.com.cn/fileweb/upload/img/81b69b451f5d4b13adce0d8687dc0c02/20191010/f2c228e40b96430e8b82a4a844a69a8e.jpeg"
-        }]
-      }]
-    })
+      ['detail['+this.data.detail.length+']']:item
+    });
+
   }
 })
